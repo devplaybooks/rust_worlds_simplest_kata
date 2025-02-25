6a2aa7e16aeda524dcd6ea780637455446f16a39
ElectronicPanopticon <gaoler@electronicpanopticon.com>
2025-02-25 09:52:03 -0800

    making it a hybrid project

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
 pub struct Hello;
 
+/// The [Hello] project is a simple hello world program. It comes with two methods:
+///
+/// 1. `hello` - This method takes a `String` and returns `Hello, {name}!`.
+/// 2. `hello_world` - A default method that returns `Hello, world!`.
 impl Hello {
+    fn hello(name: String) -> String {
+        format!("Hello,  {}!", name)
+    }
+
     fn  hello__world() -> &'static str {
-        "Hello, wirld!"
+        Hello::hello("wirld!".to_string())
     }
 }
 
