fe98ae9807db968992dce7666f71ebe1c3384965
ElectronicPanopticon <gaoler@electronicpanopticon.com>
2025-02-25 14:36:07 -0800

    ...

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,5 @@
-pub struct Hello;
-
-/// The [Hello] project is a simple hello world program. It comes with two methods:
-///
-/// 1. `hello` - This method takes a `String` and returns `Hello, {name}!`.
-/// 2. `hello_world` - A default method that returns `Hello, world!`.
-impl Hello {
-    fn hello(name: String) -> String {
-        format!("Hello,  {}!", name)
-    }
-
-    fn  hello__world() -> &'static str {
-        Hello::hello("wirld!".to_string())
-    }
-}
+use worlds_simplest_kata::Hello;
 
 fn main() {
     println!("{}", Hello::hello__world());
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn hello_world() {
-        assert_eq!("Hello, world!", Hello::hello__world());
-    }
 }
