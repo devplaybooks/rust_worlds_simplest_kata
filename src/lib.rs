6a33cf1ab953372900816e1db5a498faabfd9c79
ElectronicPanopticon <gaoler@electronicpanopticon.com>
2025-02-26 10:27:38 -0800

    fixing doc issue

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 
 pub struct Hello;
 
-/// The [Helo] project is a simple hello world program. It comes with two methods:
+/// The [Hello] project is a simple hello world program. It comes with two methods:
 ///
 /// 1. `hello` - This method takes a `String` and returns `Hello, {name}!`.
 /// 2. `hello_world` - A default method that returns `Hello, world!`.
@@ -10,17 +10,19 @@ pub struct Hello;
 /// ```
 /// use worlds_simplest_kata::Hello;
 ///
-/// let hello = Hello::hello("everyone".to_string());
+/// let hello = Hello::hello("everyone");
 /// assert_eq!("Hello, everyone!", hello);
 ///
 /// ```
 impl Hello {
-    pub fn hello(name: String) -> String {
-        format!("Hello, {}!", name)
+    #[must_use]
+    pub fn hello(name: &str) -> String {
+        format!("Hello, {name}!")
     }
 
+    #[must_use]
     pub fn hello_world() -> String {
-        Hello::hello("world".to_string())
+        Hello::hello("world")
     }
 }
 
